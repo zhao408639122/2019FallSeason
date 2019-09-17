8ab0c6b155be663cdf21e23e071a4e40b0410661
zhao408639122 <408639122@qq.com>
2019-09-17 23:50:14 +0800

    initialize

---
+++ DS/pr1/test1.cpp
@@ -0,0 +1,21 @@
+#include<cstdio>
+#include<iostream>
+#include<cstring>
+#include<string>
+
+using namespace std;
+template <class T>
+class ArrayList{
+    protected: 
+    T* _Ele;
+    int Arr_len;
+    int list_size;
+    public:
+    ArrayList(int init_L = 10);
+    ArrayList(const ArrayList<T>&)
+};
+ArrayList<T> : ArrayList(int init_L)
+int main()
+{
+
+}
